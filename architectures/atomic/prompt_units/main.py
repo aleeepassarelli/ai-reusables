12605f7cc4df3c9ddd227bb5ff50c596e995670d
Aledev <al.passarelli@gmail.com>
2025-11-11 13:50:29 -0300

    Delete architectures/atomic/prompt_units directory

--- architectures/atomic/prompt_units/main.py
+++
@@ -1,6 +0,0 @@
-from core_engineering.prompt_modular import PromptBuilder
-
-prompt = PromptBuilder()
-prompt.add("atoms/prompt_units/persona_expert.yaml")
-prompt.add("atoms/prompt_units/reasoning_chain.yaml")
-system_prompt = prompt.build()
